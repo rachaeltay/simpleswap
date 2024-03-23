1bc0e1635c36ce645d6fcb2f2ac53bafcc08dca7
rachaeltay <rtay989@gmail.com>
2024-03-23 15:15:34 +0100

    Update format

--- src/components/Exchange.jsx
+++ src/components/Exchange.jsx
@@ -1,28 +1,20 @@
 import React from 'react';
-import {
-  CircularProgress,
-} from '@mui/material';
+import { CircularProgress } from '@mui/material';
 import Form from './Form';
 import useCoinGecko from '../helper/useCoinGecko';
 
-function Exchange() {
-  const [data, error] = useCoinGecko('exchange_rates');
+const Exchange = () => {
+    const [data, error] = useCoinGecko('exchange_rates');
 
-  if (error) {
-    return (
-      <div>
-        {`Oops! There was an error: ${error.message}`}
-      </div>
-    );
-  }
+    if (error) {
+        return <div>{`Oops! There was an error: ${error.message}`}</div>;
+    }
 
-  if (!data) {
-    return <CircularProgress />;
-  }
+    if (!data) {
+        return <CircularProgress />;
+    }
 
-  return (
-    <Form coins={{ ...data.rates }} />
-  );
-}
+    return <Form coins={{ ...data.rates }} />;
+};
 
 export default Exchange;
